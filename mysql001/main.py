3f999cb716723b97ebd3f6c1a16362c1c4c8e8d8
zhs007 <sssxueren@gmail.com>
2020-02-28 20:59:34 +0800

    bugfixed

--- mysql001/main.py
+++ mysql001/main.py
@@ -11,7 +11,7 @@ ctx = SQLContext(sc)
 
 jdbcDf = ctx.read.format("jdbc").options(url=cfg['mysql']['host'],
                                        driver="com.mysql.jdbc.Driver",
-                                       dbtable="(SELECT * FROM gamelog6_api_200227 WHERE curtime >= '2020-02-27') tmp",
+                                       dbtable="(SELECT distinct(uid) as uid FROM gamelog6_api_200227 WHERE curtime >= '2020-02-27') tmp",
                                        user=cfg['mysql']['user'],
                                        password=cfg['mysql']['password']).load()
 # jdbcDf.write.saveAsTable(name='gamelog6_api_200227', mode='overwrite')
