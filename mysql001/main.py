9c6a364bc527b251467b114ef30e072c4ae827dc
zhs007 <sssxueren@gmail.com>
2020-02-28 19:14:25 +0800

    - updated mysql001

--- mysql001/main.py
+++ mysql001/main.py
@@ -11,7 +11,7 @@ ctx = SQLContext(sc)
 
 jdbcDf = ctx.read.format("jdbc").options(url=cfg['mysql']['host'],
                                        driver="com.mysql.jdbc.Driver",
-                                       dbtable="(SELECT * FROM gamelog6_api_200228 limit 0, 1000) tmp",
+                                       dbtable="(SELECT * FROM gamelog6_api_200227 WHERE curtime >= '2020-02-28') tmp",
                                        user=cfg['mysql']['user'],
                                        password=cfg['mysql']['password']).load()
 print("mysql", jdbcDf.printSchema())
