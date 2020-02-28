2c371ae62593bdaecd08aee257219e85c1dba386
zhs007 <sssxueren@gmail.com>
2020-02-28 14:19:18 +0800

    - fixed mysql001

--- mysql001/main.py
+++ mysql001/main.py
@@ -1,13 +1,15 @@
 import sys
 import yaml
 from pyspark import SparkContext
+from pyspark.sql import SQLContext
 
 f = open('config.yaml')
 cfg = yaml.load(f)
 
 sc = SparkContext("local", "mysql app")
+ctx = SQLContext(sc)
 
-jdbcDf = sc.read.format("jdbc").options(url=cfg['mysql']['host'],
+jdbcDf = ctx.read.format("jdbc").options(url=cfg['mysql']['host'],
                                        driver="com.mysql.jdbc.Driver",
                                        dbtable="SELECT * FROM gamelog6_api_200228 limit 0, 1000",
                                        user=cfg['mysql']['user'],
