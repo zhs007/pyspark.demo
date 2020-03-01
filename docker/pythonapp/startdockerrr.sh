2ad13920b1ce96b3b55fa6d65a55bcb141cc2174
zhs007 <sssxueren@gmail.com>
2020-03-01 14:13:07 +0800

    upd

--- docker/pythonapp/startdockerrr.sh
+++ docker/pythonapp/startdockerrr.sh
@@ -1,7 +1,6 @@
 docker stop spark-app-rr
 docker rm spark-app-rr
 docker run --name spark-app-rr \
-    -h spark-app \
     -v $PWD/../retentionrate:/app \
     --link spark-master:spark-master \
     -e ENABLE_INIT_DAEMON=false \
