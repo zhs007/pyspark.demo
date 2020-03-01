31dfce843e0dbea23ab27113a80db39309d605b1
zhs007 <sssxueren@gmail.com>
2020-03-01 19:15:59 +0800

    upd

--- docker/worker/startdocker.sh
+++ docker/worker/startdocker.sh
@@ -2,5 +2,6 @@ docker stop spark-worker
 docker rm spark-worker
 docker run --name spark-worker \
     --link spark-master:spark-master \
+    -v $PWD/../../rddbasic:/app \
     -e ENABLE_INIT_DAEMON=false \
     -d spark-worker
